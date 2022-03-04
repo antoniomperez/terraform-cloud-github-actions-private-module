8a67d5a5e918a75ed0fc65c4a9453725153c5e32
Antonio Márquez Pérez <antonio.marquezperez@everis.nttdata.com>
2022-03-04 11:34:26 +0100

    test passing secrets to docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "Starting publih a module in $1 Terraform organization"
+echo "Starting publishing a module in \"$1\" Terraform organization"
+echo "Storing environment variables"
+export TF_ORGANIZATION_NAME=$($1)
+echo "$TF_ORGANIZATION_NAME"
+echo "$TF_CLOUD_TOKEN"
 time=$(date)
 echo "::set-output name=time::$time"
